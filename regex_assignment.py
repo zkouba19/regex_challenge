6ac4b533c8f32ef65ccde70d8b008488d9bf5896
zkouba19 <zkouba@me.com>
2016-12-09 12:04:10 -0800

    Add files via upload

---
+++ regex_assignment.py
@@ -0,0 +1,15 @@
+Zack Kouba
+Judith Tchatchoua
+
+
+1. my_expression = r'[s][s]'
+2. my_expression = r'^b.b'
+3. my_expression = r'^b.+b'
+4. my_expression = r'b.*b'
+5. my_expression = r'[a][b]'
+6. my_expression = r'[a].*[e].*[i].*[o].*[u]'
+7. my_expression = r'[aeiou][aeiou]$'
+8. my_expression = r'^[regularexpression]*$'
+9. my_expression = r'^[^regex]*$'
+10. my_expression = r'([a-z])\1.*([a-z])\1'
+
